ac17b325140ab452f9f4013ca4c50cb02b0d8725
adamsznajder96 <adam.k.sznajder@gmail.com>
2019-11-30 13:29:51 +0100

    main

---
+++ main.m
@@ -0,0 +1,40 @@
+function main
+    path='F:\TUWien\EDBV\';
+    file=dir([path 'someone.jpg']);
+    image = imread([path file.name]);
+    
+    original_image = image;
+    
+    if ~ismatrix(image)
+        image = rgb2gray(image);
+    end
+    
+    
+    % find angle at which the image was taken
+    %angle = horizonFFT(image, 0.1);
+    %angle = mod(45+angle,90)-45;
+    % uses this angle to straighten the image
+    %res = imrotate_white(image, -angle);
+    % convert image to black and white values
+    bin_image =1-imbinarize(image, 0.9);
+
+    imshow(bin_image);
+
+    staff_lines = find_stafflines(bin_image);
+
+    if ~isempty(staff_lines)
+
+
+        % Identify positions to split into subimages for each row block
+        split_pos = get_split_positions(bin_image, staff_lines);
+        % Split into subimages
+        sub_imgs = split_images(original_image, split_pos);
+    end
+    
+    test_img = sub_imgs{1,3};
+    figure(55)
+    print_image_list(sub_imgs,22);
+    
+    decompose(test_img, 1);
+    
+end
--- split_images.m
+++ split_images.m
@@ -5,7 +5,7 @@ function [ out_im ] = split_images( in_im, split_pos )
 
     out_im = [];
     for i=1:length(split_pos)-1
-        out_im{i} = in_im(split_pos(i):split_pos(i+1),:);
+        out_im{i} = in_im(split_pos(i):split_pos(i+1),1:(size(in_im, 2)),1:3);
     end
 end
 
