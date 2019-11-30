e8795548f99d0214bf30971c0e5a1a920254f837
moel333 <43698587+moel333@users.noreply.github.com>
2019-11-30 12:06:54 +0100

    Update find_stafflines.m

--- find_stafflines.m
+++ find_stafflines.m
@@ -4,17 +4,23 @@ function [ staff_lines ] = find_stafflines( bin_image )
 %   Outputs, positions and cluster of the staff lines
     
     % Erosion of horizontal lines 
+    %se_line(1) = strel('line', length(bin_image)*0.05, 1);
+    %bin_image_ve = imerode(bin_image, se_line);
+    %bin_image=imabsdiff(bin_image,bin_image_ve);
     se_line = strel('line', length(bin_image)*0.005, 0);
     bin_image_se = imerode(bin_image, se_line);
-
-    % Plot the horizontal projection
-    plot(sum(bin_image_se,2), fliplr(1:size(bin_image_se,1)));
+    
+    
+    %DEBUGG Plot the horizontal projection
+    %imshow(bin_image_se);
+    %plot(sum(bin_image_se,2), fliplr(1:size(bin_image_se,1)));
     
     % Find locations using Horizontal projection
     [pks, locs] = findpeaks(sum(bin_image_se,2));
 
     % Remove all unrelevant peaks based on threshold
-    tresh = pks > max(pks)/4;
+    threshhold=(max(pks)-mean(pks));
+    tresh = pks > threshhold;
     locs = locs .* tresh;
     pks = pks .* tresh;
     
